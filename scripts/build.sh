453874368893d04deb77f1f4df1ad35653a157f7
Marcel Lamm <marcel.lamm@jackinpoint.net>
2020-07-20 20:05:00 +0200

    - initial commit

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,4 +6,4 @@ touch access_log
 chmod 777 access_log
 
 docker build -f scripts/docker/nginx/Dockerfile -t atomy/streamserver-nginx .
-docker tag atomy/volksstreamer-nginx:latest 613895955229.dkr.ecr.eu-central-1.amazonaws.com/atomy/streamserver-nginx:latest
+docker tag atomy/streamserver-nginx:latest 613895955229.dkr.ecr.eu-central-1.amazonaws.com/atomy/streamserver-nginx:latest
